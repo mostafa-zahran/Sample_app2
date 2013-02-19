4c3d6187391a1265a04520390b94c05b45c7e499
mostafa.k.zahran <mostafa.k.zahran@gmail.com>
2013-02-19 10:20:26 +0200

    1_commit

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ActiveRecord::Base
+  attr_accessible :email, :name, :password ,:password_confirmation
+
+  #Validatation of Name
+  validates_presence_of(:name)
+  validates_length_of(:name,:maximum => 50)
+
+  #Validatation of Email
+  validates_presence_of(:email)
+  validates_length_of(:email,:maximum => 100)
+  validates_format_of(:email,:with => /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i)
+  validates_uniqueness_of(:email,case_sensitive: false)
+  before_save{|user| user.email=email.downcase}
+
+  #Validation of Password
+  validates_presence_of(:password)
+  validates_length_of(:password,:minimum => 6)
+  has_secure_password
+
+  #Validation of Password Confirmation
+  validates_presence_of(:password_confirmation)
+end
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,41 @@
+#require 'spec_helper'
+
+#describe "StaticPages" do
+#  describe "GET /static_pages" do
+#    it "works! (now write some real specs)" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+#      get static_pages_index_path
+#      response.status.should be(200)
+#    end
+#  end
+#end
+
+
+require 'spec_helper'
+
+describe "Static pages" do
+  subject { page }
+  describe "Home page" do
+    before{visit root_path}
+    it {page.should have_selector('h1', :text => 'Sample App')}
+    it {page.should have_selector('title', :text => full_title(''))}
+    it {page.should_not have_selector('title', :text => "| Home")}
+  end
+
+  describe "Help page" do
+    before{visit help_path}
+    it {page.should have_selector('h1', :text => 'Help')}
+    it {page.should have_selector('title', :text => full_title('Help'))}
+  end
+
+  describe "About page" do
+    before{visit about_path}
+    it {page.should have_selector('h1', :text => 'About Us')}
+    it {page.should have_selector('title', :text => full_title('About Us'))}
+  end
+  describe "Contact page" do
+    before{visit contact_path}
+    it {page.should have_selector('h1', :text => 'Contact')}
+    it {page.should have_selector('title', :text => full_title('Contact'))}
+  end
+end
